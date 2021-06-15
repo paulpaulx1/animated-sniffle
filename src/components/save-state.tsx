ac1e90986684553bcc85606aa671c6729c955a8c
paul <paulmneenan@gmail.com>
2021-06-15 13:55:18 -0400

    changes favicon

---
+++ src/components/save-state.tsx
@@ -0,0 +1,11 @@
+import { store } from '../state';
+import localForage from 'localforage';
+
+interface StateSaverProps {}
+
+const StateSaver: React.FC = () => {
+  // const saveState = () => {};
+  return <div>{/* <button onClick={{ saveState }}></button> */}</div>;
+};
+
+export default StateSaver;
